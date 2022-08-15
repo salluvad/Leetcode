002afbacbfa469a7ef58485c5f0ce1470cc1abbd
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-15 17:16:14 -0400

    Update Search in 2D arr.cpp

--- Search in 2D arr.cpp
+++ Search in 2D arr.cpp
@@ -1,3 +1,19 @@
+/*
+The algorithm is a standard binary search :
+
+Initialise left and right indexes left = 0 and right = m x n - 1.
+
+While left <= right :
+
+Pick up the index in the middle of the virtual array as a pivot index : pivot_idx = (left + right) / 2.
+
+The index corresponds to row = pivot_idx // n and col = pivot_idx % n in the initial matrix, and hence one could get the pivot_element. This element splits the virtual array in two parts.
+
+Compare pivot_element and target to identify in which part one has to look for target.
+
+*/
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
