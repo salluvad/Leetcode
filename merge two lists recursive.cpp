f4e9d17510ab6e8fdbc2b13f6a24dbafa7446237
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-14 22:32:39 -0400

    Update merge two lists recursive.cpp

--- merge two lists recursive.cpp
+++ merge two lists recursive.cpp
@@ -1,20 +1,41 @@
-   // maintain an unchanging reference to node ahead of the return node.
-        ListNode prehead = new ListNode(-1);
-
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
         }
-
-        // At least one of l1 and l2 can still have nodes at this point, so connect
-        // the non-null list to the end of the merged list.
-        prev.next = l1 == null ? l2 : l1;
-
-        return prehead.next;
+        if(list2 == NULL){
+            return list1;
+        }
+        
+        ListNode *prehead = new ListNode(-1); // create a head for the merged list 
+        
+        ListNode *prev = prehead ; // prev is used for linking the nodes 
+            
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val<=list2->val){
+                prev->next = list1;
+                list1=list1->next;
+            } else {
+                prev->next = list2;
+                list2 = list2->next;
+                
+            }
+            prev = prev->next;
+        }
+        //if any of list remains 
+        prev->next = (list1==NULL)? list2:list1;
+        
+        return prehead->next;
+    }
+};
