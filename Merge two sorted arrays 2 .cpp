146a6d9501336d1aac3d42200b5e791858c98010
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-14 23:10:26 -0400

    Create Merge two sorted arrays 2 .cpp

---
+++ Merge two sorted arrays 2 .cpp
@@ -0,0 +1,26 @@
+// we need to sort such that first m elements in arr1 ans next n elements in arr2 
+// find the elemnet in arr1 which is greater thatn first element of arr2 and swap them and sort arr 2
+void merge(int arr1[], int arr2[], int n, int m) {
+  
+  int i, k;
+  for (i = 0; i < n; i++) {
+    // take first element from arr1 
+    // compare it with first element of second array
+    // if condition match, then swap
+    if (arr1[i] > arr2[0]) {
+      int temp = arr1[i];
+      arr1[i] = arr2[0];
+      arr2[0] = temp;
+    }
+
+    // then sort the second array
+    // put the element in its correct position
+    // so that next cycle can swap elements correctly
+    int first = arr2[0];
+    // insertion sort is used here
+    for (k = 1; k < m && arr2[k] < first; k++) {
+      arr2[k - 1] = arr2[k];
+    }
+    arr2[k - 1] = first;
+  }
+}
