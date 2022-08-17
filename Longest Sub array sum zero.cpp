83f0333aa40cf8eed90dcd0d212d9990b79a2420
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-17 17:48:38 -0400

    Create Longest Sub array sum zero.cpp

---
+++ Longest Sub array sum zero.cpp
@@ -0,0 +1,37 @@
+/*
+Given an array containing both positive and negative integers, we have to find the length of the longest subarray with the sum of all elements equal to zero.
+
+Example 1:
+
+Input Format: N = 6, array[] = {9, -3, 3, -1, 6, -5}
+
+Result: 5
+
+Explanation: The following subarrays sum to zero:
+{-3, 3} , {-1, 6, -5}, {-3, 3, -1, 6, -5}
+Since we require the length of the longest subarray, our answer is 5!
+*/
+
+int maxLen(int A[], int n)
+{
+    // Your code here
+    unordered_map<int,int> mpp; 
+    int maxi = 0;
+    int sum = 0; 
+    for(int i = 0;i<n;i++) {
+        sum += A[i]; 
+        if(sum == 0) {
+            maxi = i + 1; 
+        }
+        else {
+            if(mpp.find(sum) != mpp.end()) {
+                maxi = max(maxi, i - mpp[sum]); 
+            }
+            else {
+                mpp[sum] = i;
+            }
+        }   
+    }
+
+    return maxi; 
+}
