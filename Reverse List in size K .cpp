7c77febf45f6ba88b004a6953c8ca7d310759d13
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-25 15:19:01 -0400

    Update Reverse List in size K .cpp

--- Reverse List in size K .cpp
+++ Reverse List in size K .cpp
@@ -1,4 +1,9 @@
-  /**.  check TAKE you forward video for walkthrough.  https://www.youtube.com/watch?v=Of0HPkk3JgI
+1. iterative  o{1) space
+2.stack based o(k)
+
+
+
+/**.  check TAKE you forward video for walkthrough.  https://www.youtube.com/watch?v=Of0HPkk3JgI
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -40,3 +45,47 @@ private:
         return length;
   }
 };
+
+
+//simple recursive/stack based 
+
+
+struct Node* Reverse(struct Node* head, int k)
+{
+    // Create a stack of Node*
+    stack<Node*> mystack;
+    struct Node* current = head;
+    struct Node* prev = NULL;
+ 
+    while (current != NULL) {
+ 
+        // Terminate the loop whichever comes first
+        // either current == NULL or count >= k
+        int count = 0;
+        while (current != NULL && count < k) {
+            mystack.push(current);
+            current = current->next;
+            count++;
+        }
+ 
+        // Now pop the elements of stack one by one
+        while (mystack.size() > 0) {
+ 
+            // If final list has not been started yet.
+            if (prev == NULL) {
+                prev = mystack.top();
+                head = prev;
+                mystack.pop();
+            } else {
+                prev->next = mystack.top();
+                prev = prev->next;
+                mystack.pop();
+            }
+        }
+    }
+ 
+    // Next of last element will point to NULL.
+    prev->next = NULL;
+ 
+    return head;
+}
