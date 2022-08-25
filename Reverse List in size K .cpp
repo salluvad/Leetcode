760d3031cc711011d68bc2acd91d71bc01477961
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-25 15:13:35 -0400

    Create Reverse List in size K .cpp

---
+++ Reverse List in size K .cpp
@@ -0,0 +1,42 @@
+  /**.  check TAKE you forward video for walkthrough.  https://www.youtube.com/watch?v=Of0HPkk3JgI
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {      
+    const int length = getLength(head);
+    ListNode dummy(0, head);
+    ListNode* prev = &dummy;
+    ListNode* curr = head;
+
+    for (int i = 0; i < length / k; ++i) {
+      for (int j = 0; j < k - 1; ++j) {
+        ListNode* next = curr->next;
+        curr->next = next->next;
+        next->next = prev->next;
+        prev->next = next;
+      }
+      prev = curr;
+      curr = curr->next;
+    }
+
+    return dummy.next;
+    
+       
+}
+    
+private:
+  int getLength(ListNode* head) {
+        int length = 0;
+        for (ListNode* curr = head; curr!=NULL; curr = curr->next)
+              ++length;
+        return length;
+  }
+};
