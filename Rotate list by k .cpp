3cc95982ce9f4bb4f090f9effddffa76a0251f26
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-26 09:36:29 -0400

    Create Rotate list by k .cpp

---
+++ Rotate list by k .cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if(k==0){
+            return head;
+        }
+        
+        int count =1 ;
+        ListNode* curr = head;
+        
+        while(count < k and curr!=NULL){
+           
+            curr = curr->next;
+             count++;
+        }
+        
+        if(curr==NULL) {
+            // outside the list
+            return head ;   
+        }
+        
+        ListNode* KthNode = curr;
+        
+        while(curr->next!=NULL){
+            curr = curr->next;
+        }
+        
+        //connect end to first
+        curr->next = head;
+        
+        head = KthNode->next;
+        
+        KthNode->next = NULL;
+        
+        return head;
+    }
+};
