f286e0bbc16997e2a5ac35b3e9fde9071b60f0a5
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-29 18:33:18 -0400

    Update subsets all.cpp

--- subsets all.cpp
+++ subsets all.cpp
@@ -1,3 +1,30 @@
+//////////// method one works for with and with out duplicates /////////////
+class Solution {
+public:
+vector<vector<int>> subsets(vector<int>& nums) {
+    sort(nums.begin(), nums.end());   // this is not needed when you dont have duplicates 
+    vector<vector<int>> res;
+    vector<int> path;
+    dfs(nums, 0, path, res);
+    return res;
+}
+
+void dfs(vector<int>& nums, int index, vector<int>& path, vector<vector<int>>& res) {
+    res.push_back(path);
+    for (unsigned int i = index; i < nums.size(); i++) {
+        if (i > index && nums[i] == nums[i-1])    // this is not needed when you dont have duplicates 
+            continue; 
+        
+        path.push_back(nums[i]);
+        dfs(nums, i+1, path, res);
+        path.pop_back();
+    }
+}
+};
+
+
+//////////////////////// method 2 //////////////////////////
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
