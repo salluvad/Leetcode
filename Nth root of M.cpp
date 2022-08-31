bf108bf2bcd657915a211c55d6778778910854b5
salluvad <111153680+salluvad@users.noreply.github.com>
2022-08-31 16:13:32 -0400

    Create Nth root of M.cpp

---
+++ Nth root of M.cpp
@@ -0,0 +1,41 @@
+
+Time Complexity: N x log(M x 10^d).  >> d is decimal precission , eg [1,1.1,1.2,1,3,.....2,2.1,2.2.......9.9,10] , search space is M *10^d
+
+Space Complexity: O(1)
+
+
+#include <bits/stdc++.h>
+using namespace std;
+double multiply(double number, int n) {
+    double ans = 1.0;
+    for(int i = 1;i<=n;i++) {
+        ans = ans * number;
+    }
+    return ans; 
+}
+
+void getNthRoot(int n, int m) {
+    double low = 1;
+    double high = m;
+    double eps = 1e-7; 
+    
+    while((high - low) > eps) {
+        double mid = (low + high) / 2.0; 
+        if(multiply(mid, n) < m) {
+            low = mid; 
+        }
+        else {
+            high = mid; 
+        }
+    }
+    
+    cout <<n<<"th root of "<<m<<" is "<<low<<endl; 
+    
+}
+int main() {
+	int n=3, m=27; 
+	getNthRoot(n, m); 
+	return 0;
+}
+
+
