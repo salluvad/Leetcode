f8ed9d4e2efc71106363836bfa0407d1716e79ed
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-08 10:16:32 -0400

    Create level order or spiral order.cpp

---
+++ level order or spiral order.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+       queue<TreeNode *> q;
+       if (root) q.push(root);
+        while (!q.empty()) {
+          int len = q.size();
+          vector<int> level;
+          for (int i = 0;i < len;++i) {
+              TreeNode *node = q.front();
+              level.push_back(node->val);
+              if (node->left) q.push(node->left);
+              if (node->right) q.push(node->right);
+              q.pop();
+           }
+         ans.push_back(level);
+      }
+        return ans;
+    }
+};
