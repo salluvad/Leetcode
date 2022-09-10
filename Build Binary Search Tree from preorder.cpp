e9456079dc5f67e04b440cb82025de5c02e2768a
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-10 18:03:09 -0400

    Create Build Binary Search Tree from preorder.cpp

---
+++ Build Binary Search Tree from preorder.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* buildtree(vector<int>&A,int &i,int bound){
+        // base case 
+        if(i==A.size() || A[i]>bound){
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(A[i++]);
+        root->left=  buildtree(A,i ,root->val);
+        root->right =  buildtree(A,i , bound);
+        
+        return root;
+    }
+    
+    
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        // we need to keep track of upper bound ad its is a binmary search tree we will make use of that porperty ]
+        int i = 0 ;
+        return buildtree(preorder,i , INT_MAX);
+    }
+};
