132ae2a8f9285da1a7976245ca6f750e24758c5c
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-21 17:47:48 -0400

    Update Longest Substring Without Repeating Characters.cpp

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,53 @@
+/// brute force is filnd all sub strings and find if duplicates exist
+
+int solve(string str) {
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
+  {
+    unordered_set < int > set;
+    for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
+    {
+      if (set.find(str[j]) != set.end()) // if element if found so mark it as ans and break from the loop
+      {
+        maxans = max(maxans, j - i);
+        break;
+      }
+      set.insert(str[j]);
+    }
+  }
+  return maxans;
+}
+
+//// optimzed 1 , two pointer approach , keep deleting chars from left  till there are no duplicates of the current sub string
+
+int solve(string str) {
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  unordered_set < int > set;
+  int l = 0;
+  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
+  {
+    if (set.find(str[r]) != set.end()) //if duplicate element is found
+    {
+      while (l < r && set.find(str[r]) != set.end()) {
+        set.erase(str[l]);
+        l++;
+      }
+    }
+    set.insert(str[r]);
+    maxans = max(maxans, r - l + 1);
+  }
+  return maxans;
+}
+
+
+
+
 //sliding window , tow poinyer L and R , 
 //creata a map of char and index of that char as value or simple array of 256 init to -1 
 //while incrementing R , check if its not -1 in arr , then add the index to that positin , map[s[r]] = r(moving index )
