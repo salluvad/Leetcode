4849e9a81eb992b0a4af9191fd067fc1a5e5c2c4
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-21 16:46:51 -0400

    Create subarray with given XOR.cpp

---
+++ subarray with given XOR.cpp
@@ -0,0 +1,24 @@
+
+total xor from 0 to i is XR  , lets say we need to find XOR K , and Y xor K = XR , y = XR xor K , we need to find Y in the hash map 
+
+
+
+
+class Solution{
+    public:
+    int solve(vector<int> &A, int B) {
+    unordered_map<int,int>visited;
+    int cpx = 0;
+    long long c=0;
+    for(int i=0;i<A.size();i++){
+        cpx^=A[i];
+        if(cpx==B) c++;
+        int h = cpx^B;
+        if(visited.find(h)!=visited.end()){
+            c=c+visited[h];
+        }
+        visited[cpx]++;
+    }
+    return c;
+}
+};
