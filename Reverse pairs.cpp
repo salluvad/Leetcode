ee15a6de5d8da8e208c696cb222b7e64a6a4437e
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-21 10:51:41 -0400

    Update Reverse pairs.cpp

--- Reverse pairs.cpp
+++ Reverse pairs.cpp
@@ -5,23 +5,24 @@ A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2 *
 */
 
 
+
 class Solution {
 public:
     void merge(vector<int> &nums, int si, int ei, int mid, int &count){
-        int j = mid+1;
+        int j = mid;
         int i = si;
         // Main Crux of the Problem Lies Here
-        for(; i<=mid; i++){
+        for(; i<=mid-1; i++){
             while(j<=ei && nums[i]>(2*(long)nums[j])){
                 j++;
             }
-            count+=(j-(mid+1));
+            count+=(j-(mid));
         }
         i = si;
-        j = mid+1;
+        j = mid;
         int index = 0;
         vector<int> result(ei-si+1);
-        while(i<=mid && j<=ei){
+        while(i<=mid-1 && j<=ei){
             if(nums[i]<=nums[j]){
                 result[index] = nums[i];
                 i++;
@@ -31,7 +32,7 @@ public:
             }
             index++;
         }
-        while(i<=mid){
+        while(i<=mid-1){
             result[index++] = nums[i++];
         }
         while(j<=ei){
@@ -52,7 +53,7 @@ public:
         int mid = si+(ei-si)/2;
         mergeSort(nums, si, mid, count);
         mergeSort(nums, mid+1, ei, count);
-        merge(nums, si, ei, mid, count);
+        merge(nums, si, ei, mid+1, count);
         return;
     }
     
