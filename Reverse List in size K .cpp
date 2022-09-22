d38a5d36a908dc8c637003db6247b3cb3e30eac1
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-22 19:41:45 -0400

    Update Reverse List in size K .cpp

--- Reverse List in size K .cpp
+++ Reverse List in size K .cpp
@@ -1,3 +1,61 @@
+/// this one handles both cases if we want to reverse the last remainibns elements if they are less than k 
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL||head->next == NULL) return head;
+    
+    
+    
+    ListNode* dummyHead = new ListNode(0);
+    dummyHead->next = head;
+    
+    ListNode* pre = dummyHead;
+    ListNode* cur = dummyHead;
+    ListNode* next = dummyHead;
+    
+    int length = getLength(cur);
+    
+    while(next) {
+        cur = pre->next;
+        next = cur->next;
+        int toLoop = (length>k)?k:length-1;
+        for(int i=1;i<toLoop and length>=k+1 ;i++) {       // if you want to reverse last remains elemnts , remove the leghth check in inner loop
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+            next = cur->next;
+        }
+        pre = cur;
+        length -= k;
+    }
+    return dummyHead->next;
+    }
+    
+private:
+  int getLength(ListNode* head) {
+        int length = 0;
+        for (ListNode* curr = head; curr!=NULL; curr = curr->next)
+              ++length;
+        return length;
+  }
+};
+///////////////
+
+
+
+
 1. iterative  o{1) space
 2.stack based o(k)
 
