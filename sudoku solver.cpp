5264eb896a75aa91df95e9f38483840000785690
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-24 10:02:28 -0400

    Update sudoku solver.cpp

--- sudoku solver.cpp
+++ sudoku solver.cpp
@@ -7,9 +7,18 @@ bool isValid(vector < vector < char >> & board, int row, int col, char c) {
 
     if (board[row][i] == c)
       return false;
-
+    
+    int startRow = row - row % 3 ;          /// these steps are easier to understnd than the one commented below
+    int startCol = col - col % 3;
+   
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i + startRow][j + startCol] == c)
+                return false;
+    /*
     if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
       return false;
+      */
   }
   return true;
 }    
