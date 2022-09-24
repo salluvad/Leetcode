945038d8f6d9c50969e1ad10f3e759dd2a7edfd0
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-23 20:33:03 -0400

    Update Combination sum 2.cpp

--- Combination sum 2.cpp
+++ Combination sum 2.cpp
@@ -1,3 +1,33 @@
+
+>>>>>>striver
+class Solution {
+public:
+void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
+  if (target == 0) {
+    ans.push_back(ds);
+    return;
+  }
+  for (int i = ind; i < arr.size(); i++) {
+    if (i > ind && arr[i] == arr[i - 1]) continue;
+    if (arr[i] > target) break;
+    ds.push_back(arr[i]);
+    findCombination(i + 1, target - arr[i], arr, ans, ds);
+    ds.pop_back();
+  }
+}
+vector < vector < int >> combinationSum2(vector < int > & candidates, int target) {
+  sort(candidates.begin(), candidates.end());
+  vector < vector < int >> ans;
+  vector < int > ds;
+  findCombination(0, target, candidates, ans, ds);
+  return ans;
+}
+    
+};
+
+>>>>>>>>
+
+code 2: same as above
 // condition , each number can be used only once
 
 class Solution {
