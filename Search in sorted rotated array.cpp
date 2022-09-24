02233e9dd50c840ffb013c6c06a0cd1899738f0d
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-24 18:46:14 -0400

    Update Search in sorted rotated array.cpp

--- Search in sorted rotated array.cpp
+++ Search in sorted rotated array.cpp
@@ -1,3 +1,45 @@
+/////////////////////////////////////////////
+The below both solutions work 
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 , high = nums.size()-1;
+        int mid ;
+        while (low <= high) { //<--- step 2
+        int mid = (low + high) >> 1; //<----step 3
+        if (nums[mid] == target)
+          return mid; // <---step 4
+
+        if (nums[low] <= nums[mid]) { //<---step 5
+          if (nums[low] <= target && nums[mid] >= target)
+            high = mid - 1; //<---step 6 
+          else
+            low = mid + 1; //<---step 7
+          } else { //<---step 7
+          if (nums[mid] <= target && target <= nums[high])
+            low = mid + 1;
+          else
+            high = mid - 1;
+          }
+      }  
+
+      
+        return -1;
+
+
+    }     
+     
+    
+        
+    
+};
+
+
+
+
+///////////////////////////////////////////////////
 class Solution {
 public:
     int search(vector<int>& nums, int t) {
