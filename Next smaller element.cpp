00f85e7cbb3be67822b3e59538487201405d862c
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-25 14:14:08 -0400

    Update Next smaller element.cpp

--- Next smaller element.cpp
+++ Next smaller element.cpp
@@ -1,3 +1,47 @@
+//////// next smaller element to left//////
+
+vector<int> Solution::prevSmaller(vector<int> &nums) {
+      int n = nums.size();
+        vector<int> nge(n,-1);
+        stack<int> s;
+        
+        for(int i = nums.size()-1; i >= 0; i--)
+            s.push(nums[i]);
+        
+       /* for(int i = nums.size()-1; i >= 0; i--){
+            while(!s.empty() && s.top() <= nums[i])
+                s.pop();
+            
+            if(s.empty()) nge[i] = -1;
+            else nge[i] = s.top();
+            
+            s.push(nums[i]);
+        }*/
+        
+        for(int i = n-1 ; i>=0;--i){
+            
+                //remove all the smaller elements in stack
+                while(!s.empty() and s.top()>=nums[i]){
+                    s.pop();
+                }
+                //add the curr element
+       
+                if(!s.empty()) nge[i]=s.top();
+                              
+            
+                s.push(nums[i]);
+                           
+        }
+        
+        return nge;
+    }
+}
+
+
+
+
+///////////////////////////////
+
 void printNSE(int arr[], int n)
 {
 	stack<int> s;
