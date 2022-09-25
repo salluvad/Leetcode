c0b40114d15429cf4e295d58307d9ac1c2b68d44
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-25 17:30:21 -0400

    Update sliding window max.cpp

--- sliding window max.cpp
+++ sliding window max.cpp
@@ -5,14 +5,15 @@
               4. for each window add max to array
               */
         
-         deque<pair<int,int>> dq;
+         deque<pair<int,int>> dq;      /// val and index 
          vector<int> ans; 
          for(int i=0;i<nums.size();++i)
           {
             if(!dq.empty() and dq.front().second<=(i-k)) {
                dq.pop_front();
             } // out of window size
-            while(!dq.empty() and dq.back().first<nums[i]) { //Maintain elements in DSC order
+            while(!dq.empty() and dq.back().first<nums[i]) { //Maintain elements in DSC order , since there is no need of storing 
+                                                              //smaller elements if we need max of window.
                 dq.pop_back();
             }
             dq.push_back(make_pair(nums[i],i));
