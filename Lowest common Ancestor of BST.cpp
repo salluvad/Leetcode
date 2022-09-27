11017c1ac1126c9bb543193a772e6b1374f015e9
salluvad <111153680+salluvad@users.noreply.github.com>
2022-09-27 13:19:52 -0400

    Update Lowest common Ancestor of BST.cpp

--- Lowest common Ancestor of BST.cpp
+++ Lowest common Ancestor of BST.cpp
@@ -13,10 +13,35 @@ public:
         // then LCA lies in right
         else if (root->val < p->val && root->val < q->val)
                 root = root->right;
-            else break;
+            
+        else break;
 
         
     }
         return root;
     }   
 };
+
+
+///recursive
+
+/* Function to find LCA of n1 and n2.
+The function assumes that both
+n1 and n2 are present in BST */
+node* lca(node* root, int n1, int n2)
+{
+    if (root == NULL)
+        return NULL;
+ 
+    // If both n1 and n2 are smaller
+    // than root, then LCA lies in left
+    if (root->data > n1 && root->data > n2)
+        return lca(root->left, n1, n2);
+ 
+    // If both n1 and n2 are greater than
+    // root, then LCA lies in right
+    if (root->data < n1 && root->data < n2)
+        return lca(root->right, n1, n2);
+ 
+    return root;
+}
