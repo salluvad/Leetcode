492983e83f40a6620e6f9a7cbdc9fce18dd165f5
salluvad <111153680+salluvad@users.noreply.github.com>
2022-10-02 19:21:57 -0400

    Update Remove Nth node from end .cpp

--- Remove Nth node from end .cpp
+++ Remove Nth node from end .cpp
@@ -1,4 +1,4 @@
- void deleteNthNodeFromEnd(Node* head, int n)
+Node* deleteNthNodeFromEnd(Node* head, int n)
     {
         Node* fast = head;
         Node* slow = head;
@@ -6,9 +6,11 @@
         for (int i = 0; i < n; i++) {
             fast = fast->next;
         }
- 
+  // when n is the same as the length of the list, which would make the first node the target node, 
+  //and thus make it impossible to find the node before the target node. If that's the case, however, 
+  //we can just return head.next without needing to stitch together the two sides of the target node.
         if (fast == NULL) {
-            return;
+            return head->next;
         }
  
         while (fast->next != NULL) {
@@ -16,5 +18,5 @@
             slow = slow->next;
         }
         slow->next = slow->next->next;
-        return;
+        return head;
     }
