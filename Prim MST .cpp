d266cb42ab5cdc0adc8fb9dc317521a584b6e8bb
salluvad <111153680+salluvad@users.noreply.github.com>
2022-10-07 14:31:46 -0400

    Update Prim MST .cpp

--- Prim MST .cpp
+++ Prim MST .cpp
@@ -4,6 +4,22 @@
 3.we need a dist/cost array to keep min costs
 4.This is a greedy algorithm , so we pick the neighbors of lowest cost 
 
+// matric represenation is used 
+
+int selectMinVertex(vector<int>& value,vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+	for(int i=0;i<V;++i)
+	{
+		if(setMST[i]==false && value[i]<minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+	return vertex;
+}
 void findMST(int graph[V][V])
 {
 	int parent[V];		//Stores MST
