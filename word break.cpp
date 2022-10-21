4b81fc35a9a7549c83b5f3eddf15c4886b575610
salluvad <111153680+salluvad@users.noreply.github.com>
2022-10-21 19:58:37 -0400

    Update word break.cpp

--- word break.cpp
+++ word break.cpp
@@ -48,3 +48,28 @@ public:
     }
 };
 
+
+///
+/// DP bottom up , base case empty string is true 
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+       // copy the dict words to set 
+        set<string> word_set(wordDict.begin(),wordDict.end());
+        // create a dp table of size+1
+        vector<bool> dp(s.length()+1);
+        //base case 
+         dp[0]= true;
+        //two for loops . outer is partition at each word leghth 
+        //inner is to find substrings in dict
+         for (int i = 1; i <= s.length(); i++) {    // start at 1 becuase we need to break at atleast 1 index after start
+            for (int j = 0; j < i; j++) {            // end is same as len beacuse substr method need to give the whole string 
+                if(dp[j] and  word_set.find(s.substr(j, i - j)) != word_set.end() ){
+                    dp[i] = true;
+                    break;   // one true is enough to set the dp[i] so we break , we dont need to contiue cheking for all possible combinations 
+                }
+            }
+         }
+        return dp[s.length()];
+    }
+};
