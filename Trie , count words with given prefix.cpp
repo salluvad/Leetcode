8ccb3595ef13c6aa0711d7cbfa8b2f56f732e15e
salluvad <111153680+salluvad@users.noreply.github.com>
2022-10-26 20:45:13 -0400

    Create Trie , count words with given prefix.cpp

---
+++ Trie , count words with given prefix.cpp
@@ -0,0 +1,13 @@
+int countPrefix(string s)
+{
+    Trienode *curr = root;
+    int index;
+    for(int i=0; s[i]!='\0'; ++i)
+    {
+        index = s[i]-'a';
+        if(curr->child[index]==NULL || curr->child[index]->wc == 0)
+            return 0;   //No string with given prefix is present
+        curr = curr->child[index];
+    }
+    return curr->wc;
+}
