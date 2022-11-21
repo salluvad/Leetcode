334a028720d4a4f24d63609f4b8b7ef4d009734a
salluvad <111153680+salluvad@users.noreply.github.com>
2022-11-21 09:07:25 -0500

    Create Buy and sell stocks 2 .cpp

---
+++ Buy and sell stocks 2 .cpp
@@ -0,0 +1,25 @@
+ Best Time to Buy and Sell Stock II
+ 
+ You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock.
+ You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+   
+/////
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // peak valley appraoch , compare the price on prev day and add it to profit
+        int max_profit = 0 ;
+        for(int i = 1 ; i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                max_profit+=prices[i]-prices[i-1];
+            }
+        }
+        return max_profit;
+    }
+};
+
